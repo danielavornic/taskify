c47e11ad52d7701e202a80e8d80a321fd46f13fd
Daniela Vornic <daniela.vornic@gmail.com>
2022-03-08 20:35:23 +0200

    create todo and todos states functions

--- taskify/src/App.tsx
+++ taskify/src/App.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import { useState } from 'react';
 
 import InputField from './components/InputField';
 
+import { Todo } from './model-todo';
+
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      const newTodo: Todo = {
+        id: Date.now(),
+        name: todo,
+        status: 'not-started',
+      };
+      setTodos([...todos, newTodo]);
+      setTodo('');
+    }
+  };
+
   return (
     <div className='bg-slate-900 w-full h-screen'>
       <div className='container mx-auto py-10'>
         <h1 className='text-3xl text-center text-white font-semibold font-heading mb-8'>
           To do list
         </h1>
-        <InputField />
+        <InputField todo={todo} setTodo={setTodo} handleSubmit={handleSubmit} />
       </div>
     </div>
   );
--- taskify/src/components/InputField.tsx
+++ taskify/src/components/InputField.tsx
@@ -1,12 +1,25 @@
-const InputField: React.FC = () => {
+import { ChangeEvent } from 'react';
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleSubmit: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleSubmit }) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setTodo(e.target.value);
+
   return (
-    <div className='text-center'>
+    <form className='text-center' onSubmit={handleSubmit}>
       <input
         type='text'
-        placeholder='Type task…'
+        placeholder='Type a task…'
         className='w-96 py-2 px-4 text-sm text-slate-400 font-body bg-slate-800 shadow-sm rounded ease-in duration-200 outline-none hover:bg-slate-700 focus:bg-slate-700'
+        value={todo}
+        onChange={handleChange}
       />
-    </div>
+    </form>
   );
 };
 
