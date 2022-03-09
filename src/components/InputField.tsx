7884dd548ad02e7a8c1911049e2d2c0ef1ad0eb4
Daniela Vornic <daniela.vornic@gmail.com>
2022-03-09 18:24:41 +0200

    Update InputField.tsx

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, FormEvent, FC, useRef } from 'react';
 
 interface Props {
   todo: string;
@@ -6,16 +6,24 @@ interface Props {
   handleSubmit: (e: React.FormEvent) => void;
 }
 
-const InputField: React.FC<Props> = ({ todo, setTodo, handleSubmit }) => {
+const InputField: FC<Props> = ({ todo, setTodo, handleSubmit }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) =>
     setTodo(e.target.value);
 
+  const handleFormSubmit = (e: FormEvent) => {
+    handleSubmit(e);
+    inputRef.current?.blur();
+  };
+
   return (
-    <form className='text-center' onSubmit={handleSubmit}>
+    <form className='text-center' onSubmit={handleFormSubmit}>
       <input
+        ref={inputRef}
         type='text'
         placeholder='Type a task…'
-        className='w-96 py-2 px-4 text-sm text-slate-400 font-body bg-slate-800 shadow-sm rounded ease-in duration-200 outline-none hover:bg-slate-700 focus:bg-slate-700'
+        className='w-96 py-2 px-4 text-sm text-slate-400 font-body bg-slate-700 shadow-sm rounded ease-in duration-200 outline-none hover:bg-slate-600 hover:text-slate-300 focus:bg-slate-600 focus:text-slate-300'
         value={todo}
         onChange={handleChange}
       />
