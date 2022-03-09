7d418b11caceda1b60348c792521acfe53fb1014
Daniela Vornic <daniela.vornic@gmail.com>
2022-03-09 18:24:54 +0200

    add all lists and todo items

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import { useState } from 'react';
-
-import InputField from './components/InputField';
+import { FC, FormEvent, useState } from 'react';
 
 import { Todo } from './model-todo';
 
-const App: React.FC = () => {
+import InputField from './components/InputField';
+import TodoList from './components/TodoList';
+
+const App: FC = () => {
   const [todo, setTodo] = useState<string>('');
   const [todos, setTodos] = useState<Todo[]>([]);
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const statuses = ['Not started', 'In progress', 'Done'];
+
+  const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
 
     if (todo) {
       const newTodo: Todo = {
         id: Date.now(),
         name: todo,
-        status: 'not-started',
+        status: 'Not started',
       };
       setTodos([...todos, newTodo]);
       setTodo('');
@@ -23,13 +26,22 @@ const App: React.FC = () => {
   };
 
   return (
-    <div className='bg-slate-900 w-full h-screen'>
-      <div className='container mx-auto py-10'>
-        <h1 className='text-3xl text-center text-white font-semibold font-heading mb-8'>
-          To do list
-        </h1>
-        <InputField todo={todo} setTodo={setTodo} handleSubmit={handleSubmit} />
-      </div>
+    <div className='container mx-auto py-10'>
+      <h1 className='text-3xl text-center text-white font-semibold font-heading mb-8'>
+        To do list
+      </h1>
+      <InputField todo={todo} setTodo={setTodo} handleSubmit={handleSubmit} />
+      {todos.length > 0 && (
+        <div className='grid gap-5 grid-cols-1 sm:grid-cols-3 mt-10 px-8 md:px-0 lg:px-10'>
+          {statuses.map((status, idx) => (
+            <TodoList
+              status={status}
+              key={idx}
+              todos={todos.filter((todo) => todo.status === status)}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
