f657b64b6927dce14aca2f49dc94c0dfddfe85f6
Daniela Vornic <daniela.vornic@gmail.com>
2022-03-13 12:04:02 +0200

    add delete function for todo item component

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,14 +1,58 @@
 import { FC } from 'react';
+import { IoTrash } from 'react-icons/io5';
+
 import { Todo } from '../types';
+import useTodos from '../context';
 
 interface Props {
   todo: Todo;
 }
 
-const TodoItem: FC<Props> = ({ todo }) => (
-  <div className='text-white bg-slate-800 p-3 rounded shadow-sm mt-2'>
-    {todo.name}
-  </div>
-);
+const styles = {
+  todoItem: [
+    'text-white',
+    'bg-slate-800',
+    'mt-2',
+    'p-3',
+    'rounded',
+    'flex',
+    'justify-between',
+    'items-center',
+    'cursor-pointer',
+    'shadow-sm',
+    'transition-all',
+    'hover:bg-slate-700',
+    'hover:shadow-md',
+  ].join(' '),
+  todoName: [
+    'ml-2',
+    'cursor-pointer',
+    'transition-colors',
+    'hover:text-red-300',
+  ].join(' '),
+};
+
+const TodoItem: FC<Props> = ({ todo }) => {
+  const { deleteTodo } = useTodos();
+
+  const handleDeleteClick = () => {
+    const confirmDelete = () =>
+      window.confirm('Are you sure you want to delete this todo?');
+    if (confirmDelete()) deleteTodo(todo);
+  };
+
+  return (
+    <div className={styles.todoItem}>
+      <span>{todo.name}</span>
+      <span
+        className={styles.todoName}
+        title='Delete todo'
+        onClick={handleDeleteClick}
+      >
+        <IoTrash />
+      </span>
+    </div>
+  );
+};
 
 export default TodoItem;
